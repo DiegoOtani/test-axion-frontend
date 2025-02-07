676852c9a64d294f7ba708d5b4aad97337537f64
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-07 16:59:31 -0300

    style: add style to two style options in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,8 +3,12 @@
 import { ButtonProps } from "./types";
 
 const Button = ({ text, onClick, style }: ButtonProps) => {
+
+  const defaultStyle = "text-text border border-placeholder-1";
+  const gradientStyle = "bg-gradient-to-r from-primary_gradient to-second_gradient text-foreground";
+
   return <button 
-    className={``}
+    className={`w-full py-[18px] font-semibold text-md font-text ${style === 'default' ? defaultStyle : gradientStyle} hover:opacity-85`}
       onClick={onClick}
     >
     {text}
