6757b9b5a58e1ff6d7b9c7ed371c98ca71c516ff
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-07 17:54:24 -0300

    style: add style in InputArea component

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -2,21 +2,24 @@ import { InputProps } from "./types";
 import Image from "next/image";
 
 const InputArea = ({ name, placeholder, onChange, value, iconImg }: InputProps) => {
-  return <section>
-    <span>{name}</span>
-    <div>
+  return <section className="flex flex-col gap-2">
+    <span className="text-md font-semibold text-text">
+      {name}
+    </span>
+    <div className="flex py-[18px] px-5 border border-text ">
       <input 
+        className="w-full placeholder:text-placeholder text-text text-md font-light"
         type="text" 
         name={name}
         value={value} 
         placeholder={placeholder} 
         onChange={onChange}
       />
-      <Image 
+      <Image className="w-5 h-5"
         alt={name} 
         src={iconImg} 
         width={20}
-        height={55}
+        height={20}
       />
     </div>
   </section>
