8d61bb0970a0e76d04344c17202d210f8830a46b
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-07 17:08:54 -0300

    refactor: removing Title component to use logo.png in your place

--- src/components/Title/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TitleProps } from "./types";
-
-const Title = ({ text }: TitleProps) => {
-  return <h1 className="text-xll font-bold text-text font-title tracking-[14px]">
-    {text}
-  </h1>
-}
-
-export default Title;
