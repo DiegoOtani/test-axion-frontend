f629ffc25bb1febb26b227ada50ae50fa6f83cd7
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-09 14:06:09 -0300

    fix: adjust ListTitle import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import Foods from "@/components/Foods";
 import Loading from "@/components/Loading";
-import ListTilte from "@/components/ListTitle";
+import ListTitle from "@/components/ListTitle";
 
 export default function FoodsPage() {
   return (
     <section className="px-[17.36%] flex flex-col">
-      <ListTilte title="FOODS"/>
+      <ListTitle title="FOODS"/>
       
       <Suspense fallback={<Loading />}>
         <Foods />
