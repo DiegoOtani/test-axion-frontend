2277e7781b881fbf24a7b6904787233d5fc3a7b1
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-09 15:56:39 -0300

    feat: make login action create a jwt cookie

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 const loginAction = async (email: string, password: string) => {
 
   const response = await fetch(`${process.env.BACKEND_API_URL}/auth/local`, {
@@ -15,7 +17,13 @@ const loginAction = async (email: string, password: string) => {
 
   if(!response.ok) return { error: "Usuário ou senha incorretos!"};
 
-  const data = await response.json()
+  const data = await response.json();
+
+  (await cookies()).set("jwt", data.jwt, {
+    httpOnly: true,
+    path: "/",
+    maxAge: 60 * 60,
+  })
   return data;
 }
 
