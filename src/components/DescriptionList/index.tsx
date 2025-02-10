12fc92c3e3da81724e72cea5a72338e03940d1e3
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-10 16:54:55 -0300

    style: add justify-between in DescriptionList component

--- src/components/DescriptionList/index.tsx
+++ src/components/DescriptionList/index.tsx
@@ -8,7 +8,7 @@ const DescriptionList: React.FC<DescriptionListProps> = ({ data }) => {
   return (
     <ul >
       {Object.entries(data).map(([key, value]) => (
-        <li className="text-md text-text font-light flex gap-3"
+        <li className="text-md text-text font-light flex justify-between gap-3"
           key={key}
         >
           <span className="text-text text-md font-bold">
