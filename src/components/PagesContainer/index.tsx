50031a5d57c0cfc298c0ff1f573ed3a544119453
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-10 17:57:24 -0300

    style: adjust padding with Header fixed

--- src/components/PagesContainer/index.tsx
+++ src/components/PagesContainer/index.tsx
@@ -1,6 +1,6 @@
 const PagesContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="px-[17.36%] flex flex-col">
+    <section className="px-[17.36%] pt-24 flex flex-col min-h-[1300px]">
       {children}
     </section>
   );
