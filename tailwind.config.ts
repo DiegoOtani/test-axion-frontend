53166ba47540099c408d3571181394f2465cb93b
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-10 14:44:51 -0300

    style/fix: removing font variable from tailwind and fix sans-serif font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,9 +23,6 @@ export default {
         xl: "var(--font-size-xl)",
         xll: "var(--font-size-2xl)",
       },
-      fontFamily: {
-        text: ["var(--font-family-text)"],
-      },
       boxShadow: {
         custom: '0px 2px 7px 3px rgba(0, 0, 0, 0.1)'
       }
