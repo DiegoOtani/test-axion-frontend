23f65a0b102752e8a01e50f2a6e90f7130970d95
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-10 17:56:56 -0300

    style: use useState and useEffect to change Header shadow when scroll and change to fixed component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import NavOption from "../NavOption";
 import Link from "next/link";
@@ -5,26 +8,43 @@ import { usePathname } from "next/navigation";
 
 const Header = () => {
   const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  return <header className="flex py-7 px-[250px] items-center justify-between bg-foreground shadow-custom">
-    <Link href={"/"}>
-      <Image
-        className="w-[230px] h-auto" 
-        alt="ORANGE logo" 
-        src={"/logo.png"} 
-        width={230} 
-        height={40} 
-        priority
-      />
-    </Link>
-    <nav>
-      <ul className="flex gap-14">
-        <NavOption active={pathname === "/"} href="/" text="FOODS"/>
-        <NavOption active={pathname === "/people"} href="/people" text="PEOPLE"/>
-        <NavOption active={pathname === "/place"} href="/place" text="PLACES"/>
-      </ul>
-    </nav>
-  </header>
-}
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`
+          fixed top-0 left-0 w-full flex py-8 px-[250px] items-center justify-between bg-foreground duration-300 z-10 ${
+        isScrolled ? "shadow-none border border-black border-opacity-10" : "shadow-custom border-none"
+      }`}
+    >
+      <Link href={"/"}>
+        <Image
+          className="w-[230px] h-auto"
+          alt="ORANGE logo"
+          src={"/logo.png"}
+          width={230}
+          height={40}
+          priority
+        />
+      </Link>
+      <nav>
+        <ul className="flex gap-14">
+          <NavOption active={pathname === "/"} href="/" text="FOODS" />
+          <NavOption active={pathname === "/people"} href="/people" text="PEOPLE" />
+          <NavOption active={pathname === "/place"} href="/place" text="PLACES" />
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
