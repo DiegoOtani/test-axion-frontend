ea6f35c3bf37ae185e7729589dbb1963b4202bc6
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-10 15:15:03 -0300

    feat: add DescriptionList component

---
+++ src/components/DescriptionList/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type DescriptionListProps = {
+  data: Record<string, string>;
+};
+
+const DescriptionList: React.FC<DescriptionListProps> = ({ data }) => {
+  return (
+    <ul >
+      {Object.entries(data).map(([key, value]) => (
+        <li className="text-md text-text font-light flex gap-3"
+          key={key}
+        >
+          <span className="text-text text-md font-bold">
+            {key}:
+          </span> 
+          <span>
+            {String(value)}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default DescriptionList;
