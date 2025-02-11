030c1231c84282454ad6d0a6bbbb426181013a01
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 12:40:35 -0300

    style: changin Loading font and postion

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
-  return <div className="w-full flex items-center justify-center">
-    <span>Loading...</span>
+  return <div className="w-full flex items-center justify-center h-[400px]">
+    <span className="text-text font-bold">Loading...</span>
   </div>;
 }
 
